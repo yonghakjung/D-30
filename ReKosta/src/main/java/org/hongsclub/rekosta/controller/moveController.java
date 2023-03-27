c0f856b27066b83c4fb9ac5924ef79f7ab2a93ca
USER <heperion14@gmail.com>
2023-03-27 13:51:14 +0900

    홍주영 배너이미지수정 배너 클릭 시 active되도록 수정

--- ReKosta/src/main/java/org/hongsclub/rekosta/controller/moveController.java
+++ ReKosta/src/main/java/org/hongsclub/rekosta/controller/moveController.java
@@ -1,28 +1,36 @@
 package org.hongsclub.rekosta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class moveController {
+	private boolean flag = true;
+	
 	@GetMapping(value={"/home","/","/index"})
-	public String moveHome() {
+	public String moveHome(Model model) {
+		model.addAttribute("activeHome",flag);
 		return "content/index";
 	}
 	@GetMapping("/styleDemo")
-	public String moveStyleDemo() {
+	public String moveStyleDemo(Model model) {
+		model.addAttribute("activeStyleDemo",flag);
 		return "content/style-demo";
 	}
 	@GetMapping("/fullWidth")
-	public String moveFullWidth() {
-		return "content/full-Width";
+	public String moveFullWidth(Model model) {
+		model.addAttribute("activeFullWidth", flag);
+		return "content/full-width";
 	}
 	@GetMapping("/3columns")
-	public String move3Columns() {
+	public String move3Columns(Model model) {
+		model.addAttribute("active3Columns",flag);
 		return "content/3-columns";
 	}
 	@GetMapping("/gallery")
-	public String moveGallery() {
+	public String moveGallery(Model model) {
+		model.addAttribute("activeGallery",flag);
 		return "content/gallery";
 	}
 }
