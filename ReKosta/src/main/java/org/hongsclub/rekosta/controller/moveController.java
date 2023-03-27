c1765d01e3aeebd244f961d0377c37bfc9ef62f5
USER <heperion14@gmail.com>
2023-03-27 15:14:24 +0900

    홍주영 헤더 클릭시 해당 목록 active되도록 수정 헤더 목록 및 dropdown목록 추가 moveController추가

--- ReKosta/src/main/java/org/hongsclub/rekosta/controller/moveController.java
+++ ReKosta/src/main/java/org/hongsclub/rekosta/controller/moveController.java
@@ -7,30 +7,90 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class moveController {
 	private boolean flag = true;
-	
+	//홈
 	@GetMapping(value={"/home","/","/index"})
-	public String moveHome(Model model) {
-		model.addAttribute("activeHome",flag);
+	public String moveHome() {
 		return "content/index";
 	}
-	@GetMapping("/styleDemo")
-	public String moveStyleDemo(Model model) {
-		model.addAttribute("activeStyleDemo",flag);
-		return "content/style-demo";
+	//협회소개
+	@GetMapping("/moveIntroduceOrganization")
+	public String moveIntroduceOrganization(Model model) {
+		model.addAttribute("activeIntroduceAndRegister",flag);
+		return "content/introduceOrganization";
 	}
-	@GetMapping("/fullWidth")
-	public String moveFullWidth(Model model) {
-		model.addAttribute("activeFullWidth", flag);
-		return "content/full-width";
+	@GetMapping("/moveIntroduceRegisterMembership")
+	public String moveRegisterMembership(Model model) {
+		model.addAttribute("activeIntroduceAndRegister",flag);
+		return "content/introduceRegisterMembership";
 	}
-	@GetMapping("/3columns")
-	public String move3Columns(Model model) {
-		model.addAttribute("active3Columns",flag);
-		return "content/3-columns";
+	//교육훈련
+	@GetMapping("/moveEmployed")
+	public String moveEmployed(Model model) {
+		model.addAttribute("activeEmployed",flag);
+		return "content/employed";
 	}
-	@GetMapping("/gallery")
-	public String moveGallery(Model model) {
-		model.addAttribute("activeGallery",flag);
-		return "content/gallery";
+	@GetMapping("/moveIncumbent")
+	public String moveIncumbent(Model model) {
+		model.addAttribute("activeEmployed",flag);
+		return "content/incumbent";
+	}
+	@GetMapping("/moveRecruits")
+	public String moveRecruits(Model model) {
+		model.addAttribute("activeEmployed",flag);
+		return "content/recruits";
+	}
+	//구인구직
+	@GetMapping("/moveJobOffer")
+	public String moveJobOffer(Model model) {
+		model.addAttribute("activeJobOffer",flag);
+		return "content/joboffer";
+	}
+	@GetMapping("/moveJobSearch")
+	public String moveJobSearch(Model model) {
+		model.addAttribute("activeJobOffer",flag);
+		return "content/jobsearch";
+	}
+	//시험인증
+	@GetMapping("/moveTest")
+	public String moveTest(Model model) {
+		model.addAttribute("activeTest",flag);
+		return "content/test";
+	}
+	//sw역량진단&개발컨설팅
+	@GetMapping("/moveSWAbilityTest")
+	public String moveSWAbilityTest(Model model) {
+		model.addAttribute("activeSWAbilityTest", flag);
+		return "content/swabilitytest";
+	}
+	@GetMapping("/moveSSPL")
+	public String moveSSPL(Model model) {
+		model.addAttribute("activeSWAbilityTest",flag);
+		return "content/sspl";
+	}
+	//디지털비즈니스지원센터
+	@GetMapping("/moveDigitalBusiness")
+	public String moveDigitalBusiness(Model model) {
+		model.addAttribute("activeDigitalBusiness",flag);
+		return "content/digitalbusiness";
+	}
+	@GetMapping("/moveVideoLecture")
+	public String moveVideoLecture(Model model) {
+		model.addAttribute("activeDigitalBusiness",flag);
+		return "content/videolecture";
+	}
+	//고객센터
+	@GetMapping("/moveNoticeBoard")
+	public String moveNoticeBoard(Model model) {
+		model.addAttribute("activeNoticeBoard",flag);
+		return "content/digitalbusiness";
+	}
+	@GetMapping("/moveKostaSympathy")
+	public String moveKostaSympathy(Model model) {
+		model.addAttribute("activeNoticeBoard",flag);
+		return "content/kostasympathy";
+	}@GetMapping("/moveMembershipAdvertise")
+	public String moveMembershipAdvertise(Model model) {
+		model.addAttribute("activeNoticeBoard",flag);
+		return "content/membershipadvertise";
 	}
 }
