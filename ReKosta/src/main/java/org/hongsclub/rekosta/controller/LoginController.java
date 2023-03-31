f294b07db70897429be330b64084320c029fe83a
scardy14 <heperion14@gmail.com>
2023-03-31 15:02:03 +0900

    홍주영 기업 및 개인 회원가입 페이지 생성 및 컨트롤러 작업

--- ReKosta/src/main/java/org/hongsclub/rekosta/controller/LoginController.java
+++ ReKosta/src/main/java/org/hongsclub/rekosta/controller/LoginController.java
@@ -1,7 +1,9 @@
 package org.hongsclub.rekosta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -20,5 +22,15 @@ public class LoginController {
 	@GetMapping("/moveRegisterChoice")
     public String moveRegisterChoice() {
         return "login/registerchoice";
+    }
+	@PostMapping("/registerCompany")
+    public String registerCompany(Model model) {
+		model.addAttribute("result",true);
+        return "login/registercompanyresult";
+    }
+	@PostMapping("/registerIndividual")
+    public String registerIndividual(Model model) {
+		model.addAttribute("result",true);
+        return "login/registerindividualresult";
     }
 }
