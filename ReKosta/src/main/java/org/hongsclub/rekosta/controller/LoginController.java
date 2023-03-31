f93c270a97a81a4fae8a5ec8ce1727038cbaeeba
scardy14 <heperion14@gmail.com>
2023-03-31 13:39:11 +0900

    로그인 작업 베이스

--- ReKosta/src/main/java/org/hongsclub/rekosta/controller/LoginController.java
+++ ReKosta/src/main/java/org/hongsclub/rekosta/controller/LoginController.java
@@ -5,8 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-	@GetMapping("/loginForm")
-    public String loginForm() {
+	@GetMapping("/moveLoginForm")
+    public String moveLoginForm() {
         return "login/loginForm";
+    }
+	@GetMapping("/moveCompanyRegisterForm")
+    public String moveCompanyRegisterForm() {
+        return "login/companyregisterform";
+    }
+	@GetMapping("/moveIndividualRegisterForm")
+    public String moveRegisterForm() {
+        return "login/individualregisterform";
+    }
+	@GetMapping("/moveRegisterChoice")
+    public String moveRegisterChoice() {
+        return "login/registerchoice";
     }
 }
