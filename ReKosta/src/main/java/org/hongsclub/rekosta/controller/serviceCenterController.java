fae345e40fe5091265271612c2fe7c61ec5b4823
ParkJinSoo189 <xnqlwlrh@naver.com>
2023-03-31 15:40:36 +0900

    pjs0331 - 중복 html삭제 , 회원사 협력사 글쓰기 검색창 디테일글 확인

--- ReKosta/src/main/java/org/hongsclub/rekosta/controller/serviceCenterController.java
+++ ReKosta/src/main/java/org/hongsclub/rekosta/controller/serviceCenterController.java
@@ -1,14 +1,19 @@
 package org.hongsclub.rekosta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import groovyjarjarpicocli.CommandLine.Model;
 
 @Controller
 public class serviceCenterController {
+	private boolean flag=true;
 	@GetMapping("/membershipadvertiseDetail")
 	public String membershipadvertiseDetail(Model model) {
+		model.addAttribute("activeNoticeBoard2", flag);
 		return "content/servicecenter/membershipadvertiseDetail";
+	}
+	@GetMapping("membershipadvertiseWrite")
+	public String membershipadvertiseWrite() {
+		return "content/servicecenter/membershipadvertiseWrite";
 	}
 }
