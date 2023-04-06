c48586b1ab4ebcdb066af6015153cf6bba2287cb
jjiyoungg <jh99165@naver.com>
2023-04-06 10:13:34 +0900

    지영 - 고객센터 - kosta 동정, 알림마당 글쓰기,  찾기 등

--- ReKosta/src/main/java/org/hongsclub/rekosta/controller/serviceCenterController.java
+++ ReKosta/src/main/java/org/hongsclub/rekosta/controller/serviceCenterController.java
@@ -26,13 +26,37 @@ public class serviceCenterController {
 	public String resultMembershipAdvertise() {
 		return "content/servicecenter/resultMembershipAdvertise";
 	}
+	// 알림마당 글쓰기
+	@GetMapping("moveWriteServiceCenter")
+	public String moveWriteServiceCenter(Model model) {
+		model.addAttribute("activeNoticeBoard",flag);
+		return "content/servicecenter/writeservicecenter";
+	}
 	@GetMapping("servicecenterDetail")
 	public String servicecenterDetail() {  
 		return "content/servicecenter/servicecenterDetail";
+	}
+	// kosta 동정 글쓰기
+	@GetMapping("moveWriteKostaSympathy")
+	public String moveWriteKostaSympathy(Model model) {
+		model.addAttribute("activeKostaSympathy",flag);
+		return "content/servicecenter/kostasympathywrite";
 	}
 	@GetMapping("kostasympathyDetail")
 	public String kostasympathyDetail() {
 		return "content/servicecenter/kostasympathyDetail";
 	}
-	
+	@GetMapping("kostasympathyWrite")
+	public String kostasympathyWrite() {
+		return "content/servicecenter/kostasympathyWrite";
+	}
+	@PostMapping("WriteServiceCenter")
+	public String WriteServiceCenter() {
+		return "redirect:resultservicecenter";
+	}
+	@PostMapping("WriteKostaSympathy")
+	public String writekostasympathy() {
+		return "redirect:resultkostasympathy";
+	}
+
 }
